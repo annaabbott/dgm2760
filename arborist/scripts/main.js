46036993ae558a9a6bd1a35ca266115821d0a3d8
Anna Abbott <annaabbottdesign@gmail.com>
2020-09-28 19:39:10 -0600

    completed header with background image

---
+++ arborist/scripts/main.js
@@ -0,0 +1,3 @@
+document.querySelector("header >h1").innerText = "The Arborist";
+document.querySelector("header > h2").innerText = "The best time to plant a tree was 20 years ago. The second best time is now.";
+
