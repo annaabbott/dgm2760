d3ab5a3a72e6e6ab7feac5e8f4162e99103984c1
Anna Abbott <annaabbottdesign@gmail.com>
2020-10-04 18:30:27 -0600

    set up html

--- nonsense-story/scripts/main.js
+++ nonsense-story/scripts/main.js
@@ -1,3 +1,14 @@
-document.querySelector("header >h1").innerText = "Company Name";
+document.querySelector("header >h1").innerText = "Nonsense Story";
 document.querySelector("header > h2").innerText = "Company Tagline";
 
+// build nounArray
+// build verbArray
+// build adjArray
+
+//function tellStory 
+    // build a template string, substituting array values by index.
+    // all changes (adding suffixes, etc) can be added in the template string.
+
+// attach the reference to the DOM with the id.
+
+//One function to rule them all.  That's it!
