3794a6050b8a9d497c4863b0b1e33d42bff5984d
Anna Abbott <annaabbottdesign@gmail.com>
2020-11-06 15:57:32 -0700

    created files for hotel-booking page

---
+++ hotel-booking/scripts/main.js
@@ -0,0 +1,3 @@
+document.querySelector("header >h1").innerText = "2020 Virtual Vacations";
+document.querySelector("header > h2").innerText = "While the virus keeps us home, we can still pretend";
+
