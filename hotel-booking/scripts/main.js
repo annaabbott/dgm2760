6cd1f0c286e6286ec981c191e250800412bdab3b
Anna Abbott <annaabbottdesign@gmail.com>
2020-11-07 16:21:54 -0700

    set up json and fetch

--- hotel-booking/scripts/main.js
+++ hotel-booking/scripts/main.js
@@ -1,3 +1,17 @@
 document.querySelector("header >h1").innerText = "2020 Virtual Vacations";
 document.querySelector("header > h2").innerText = "While the virus keeps us home, we can still pretend";
 
+async function getHotelData() {
+    try {
+        const response = await fetch("./hotel.json");
+        return await response.json();
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+let hotelData = {};
+
+getHotelData().then(data => {
+    return hotelData = data;
+});
