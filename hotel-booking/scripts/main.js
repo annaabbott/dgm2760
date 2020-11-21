81d5c1ef955231500c6e9deef0cc60d9ccea5fef
Anna Abbott <annaabbottdesign@gmail.com>
2020-11-21 16:22:57 -0700

    completed javascript

--- hotel-booking/scripts/main.js
+++ hotel-booking/scripts/main.js
@@ -46,6 +46,12 @@ function hotelInfo(event) {
         return event.target.id === hotel.id
     });
     console.log(hotelChoice);
-    document.querySelector("#hotelDisplay").textContent = `${hotelChoice.name}`;
+    document.querySelector("#hotelImg").setAttribute("src", hotelChoice.picture);
+    document.querySelector("#hotelName").textContent = `${hotelChoice.name}`;
+    document.querySelector("#hotelRooms").textContent = `Rooms: ${hotelChoice.rooms} total ${hotelChoice.roomTypes[0]}, ${hotelChoice.roomTypes[1]}, and/or ${hotelChoice.roomTypes[2]} (Call for availability).`;
+    document.querySelector("#gym").textContent = `Gym available? ${hotelChoice.gym}`;
+    document.querySelector("#meeting").textContent = `Conference Room? ${hotelChoice.meeting}`;
+    document.querySelector("#address").textContent = `Location: ${hotelChoice.address}`;
+    document.querySelector("#phone").textContent = `Phone: ${hotelChoice.phone}`;
 }
 
